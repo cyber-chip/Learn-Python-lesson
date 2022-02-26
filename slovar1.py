7d8461b2dacafd0a0be927d6028d87868ffdc9a0
Qubit8 <qubit8@MacBook-Air-Qubit8.local>
2022-02-26 09:50:19 +0500

    Создание словаря и изменения значения

---
+++ slovar1.py
@@ -0,0 +1,21 @@
+#Задание1
+#Создайте словарь:
+#{"city": "Москва", "temperature": "20"}
+#Выведите на экран значение ключа city
+#Уменьшите значение "temperature" на 5
+#Выведите на экран весь словарь
+
+
+#Создайте словарь: {"city": "Москва", "temperature": "20"}
+goroda = {"city": "Москва", "temperature": "20"}
+
+#Выведите на экран значение ключа city
+print(goroda["city"])
+
+#Уменьшите значение "temperature" на 5
+goroda['temperature'] = int(goroda['temperature']) - 5
+
+#Выведите на экран весь словарь
+print(goroda)
+
+
